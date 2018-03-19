d32b55895331c0a439a6be8a73a397fb749ddcfd
Okemwaa <okemwa.monicah@gmail.com>
2018-03-19 16:59:59 +0300

    initializing repo

---
+++ script.js
@@ -0,0 +1,6 @@
+let card1 = "Ace of Spades",
+	card2 = "Ten of Hearts";
+
+console.log("Welcome to BlackJack");
+console.log("You have been dealt " + card1);
+console.log("You have been dealt " + card2);
