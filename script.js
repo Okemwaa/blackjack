9df3b52c72446d4039f230c8470c5237c840bb87
Okemwaa <okemwa.monicah@gmail.com>
2018-03-22 17:49:32 +0300

    create card function

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-let deck =[ "Ace of Spades",
-			"Two of Spades",
-			"Three of Spades"
-];
 
-let playerCards = [  deck[0], deck[2] ];
 
-console.log("Welcome to BlackJack");
-console.log("You have been dealt: ")
-console.log("	" + playerCards[0]);
-console.log("	" + playerCards[1]);
+let suits =[ 'Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values =['Ace', 'King', 'Queen', 'Jack', 'Ten', 
+	'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 
+	'Three', 'Two'];
+
+
+function createDeck() {
+	let deck = [];
+	for (let suitsIdx = 0; suitsIdx < suits.length; suitsIdx++) {
+		for (let valuesIdx = 0; valuesIdx < values.length; valuesIdx++) {
+			let card = {
+				suit: suits[suitsIdx],
+				value: values[valuesIdx]
+			}
+			deck.push(card);
+		}
+	}
+	return deck;
+}	
+
+function getCardString(card) {
+	return card.value + ' of ' + card.suit;
+}
+function getNextCard() {
+	return deck.shift();
+}
+let deck = createDeck();
+
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log('Welcome to Blackjack');
+console.log('You are dealt: ');
+console.log(' ' + getCardString(playerCards[0]));
+console.log(' ' + getCardString(playerCards[1]));
